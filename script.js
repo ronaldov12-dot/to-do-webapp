6197f429aaf82742f1bd2ae5a5d23a6ef4ed4796
ronaldov12-dot <anstinronaldo1@gmail.com>
2026-04-16 21:03:25 +0530

    Initial commit - v1

---
+++ script.js
@@ -0,0 +1,52 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTasks() {
+    const list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            <span onclick="toggleTask(${index})" 
+            class="${task.completed ? 'completed' : ''}">
+                ${task.text}
+            </span>
+            <button onclick="deleteTask(${index})">X</button>
+        `;
+
+        list.appendChild(li);
+    });
+}
+
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const text = input.value.trim();
+
+    if (text === "") return;
+
+    tasks.push({ text: text, completed: false });
+    input.value = "";
+
+    saveTasks();
+    renderTasks();
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    saveTasks();
+    renderTasks();
+}
+
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    saveTasks();
+    renderTasks();
+}
+
+// Load tasks when page opens
+renderTasks();
